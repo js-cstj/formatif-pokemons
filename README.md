b41951e940dd06f2c57c04bbea0b534d0fc8c1bb
bobanum <mboudrea@cstj.qc.ca>
2021-12-09 16:58:47 -0500

    Préparation pour une épreuve complète

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
-# application-base
-Application de base pour débuter un projet
+# Citations
+Reproduisez l'application montrée dans la démo du professeur.
 
-## Description
-Description à venir
+## Fonctionnement
+Lorsque l'usager arrive dans le site, des Pokémons aléatoires sont affichés dans la page. Par la suite, l'usager peut faire une recherche à l'aide du formulaire prévu à cet effet. L'affichage est alors remplacé par les Pokémons satisfaisant le critère de recherche.
 
-## Particularités
+## Le rite de passage
+Les fichiers `resultatAlea.html` et `resultatRecherche.html` présentent une version HTML de la page terminée. Servez-vous-en pour reproduire avec des commandes Javascript le HTML de base de l'application. Gardez à l'esprit que le code doit être placé dans des fonctions spécifiques (voir ci-dessous).
 
-- Compléter
+## Les fonctions
+Pour l'épreuve vous devez créer les 5 fonctions suivantes :
+- html_listePokemons
+- html_pokemon
+- html_nom
+- html_description
+- html_image
 
-## Variantes et ajouts possibles
+**Note**: Si vous avez de la difficulté avec les fonctions, vous pouvez sauver les meubles en complétant l'épreuve complètement dans la fonction `main`
 
-- Compléter
+## L'API
+Les données doivent provenir de l'adresse suivante :
+- https://mboudrea.tim-cstj.ca/epreuve3/pokemon.php
+- https://mboudrea.tim-cstj.ca/epreuve3/pokemon.php?mot=pikachu _(à modifier en fonction de la recherche demandée)_
+
+Vous pouvez vous référer aux fichiers `exempleAlea.json` et `exempleRecherche.json` pour la structure du document retourné par l'API. Vous __ne devez pas__ utiliser ces fichiers dans la version remise.
+
+## Le SCSS
+L'imbrication d'une partie du fichier `style.scss` a été omise. Vous devez refaire l'imbrication des règles qui se trouvent à la fin du fichier.
+
+## Les tâches
+1. Ajouter la fonction responsable de l'acquisition des données provenant de l'API;
+1. Utiliser cette fonction pour récupérer les données aléatoires (voir les commentaires dans le `main`);
+1. Programmer les fonctions demandées (rite de passage) avec ou sans les données réelles;
+1. Ajouter les données réelles si ce n'est pas déjà fait;
+1. Ajouter et compléter l'événement du formulaire (voir les commentaires dans le `main`);
+1. Arranger le SCSS comme décrit plus haut;
+
+## Remise
+- Travail individuel
+- À faire en classe
+- Durée : 3h
+- Compte dans l'évaluation synthèse de cours
+- Compte pour 15% de la note finale
+- Renommer le dossier NOMP0123456 AVANT de le zipper et de le remettre sur https://remise.cstj.qc.ca (epreuve_synthese)
+
+
+---
+### Provenance du contenu textuel
+Les donnees proviennent du site https://pokeapi.co/
--- src/App.js
+++ src/App.js
@@ -16,17 +16,35 @@ export default class App {
 		document.getElementById("search").addEventListener("submit", e => {
 			// console.log(e.currentTarget.mot.value);
 			var mot = e.currentTarget.mot.value;
-			while (app.firstChild !== null) {
-				app.firstChild.remove();
-			}
+			// while (app.firstChild !== null) {
+			// 	app.firstChild.remove();
+			// }
+			this.reset();
 			this.chargerJson('http://mboudrea.tim-cstj.ca/epreuve3/pokemon.php?mot='+mot).then(donnees => {
-				console.log(donnees);
+				// console.log(donnees);
 				var h1 = app.appendChild(document.createElement("h1"));
 				h1.innerHTML = "Mes Pokémons trouvés";
 				app.appendChild(this.html_listePokemons(donnees.pokemons));
 			});
 		});
 	}
+	/**
+	 * Fonction reset. Enlève le contenu de la zone d'affichage
+	 * @note Cette fonction est complète.
+	 * @returns {undefined} Ne retourne rien
+	 */
+	 static reset() {
+		var app = document.getElementById("app");
+		while (app.firstChild) {
+			app.firstChild.remove();
+		}
+	}
+
+	/**
+	 * Fonction html_listePokemons. Retourne le HTML de l'ensemble des Pokémons en fonction du tableau donné
+	 * @param {array} tPokemons Tableau contenant des objets avec le nom du Pokémon, sa description, son image...
+	 * @returns HTMLElement Le div#pokemons
+	 */
 	static html_listePokemons(tPokemons) {
 		// console.log(tPokemons);
 		var resultat = document.createElement("div");
@@ -34,25 +52,73 @@ export default class App {
 		for (let i = 0; i < tPokemons.length; i++) {
 			const objPokemon = tPokemons[i];
 			// console.log(objPokemon);
-			var divPokemon = resultat.appendChild(document.createElement("div"));
-			divPokemon.classList.add("pokemon");
-			var h2 = divPokemon.appendChild(document.createElement("h2"));
-			h2.innerHTML = objPokemon.nom;
-			var p = divPokemon.appendChild(document.createElement("p"));
-			p.innerHTML = objPokemon.description.replace(/<br\/>/g, " ");
-			var divImage = divPokemon.appendChild(document.createElement("div"));
-			divImage.classList.add("image");
-			var img = divImage.appendChild(document.createElement("img"));
-			img.alt = objPokemon.nom;
-			if (objPokemon.image_url === "") {
-				img.src = "http://mboudrea.tim-cstj.ca/epreuve3/pokemons/pokeball.png";
-			} else {
-				img.src = objPokemon.image_url;
-			}
+			resultat.appendChild(this.html_pokemon(objPokemon));
 		}
 		return resultat;
 	}
-	static chargerJson(url) {
+	/**
+	 * Fonction html_pokemon. Retourne le div.pokemon d'un Pokémon
+	 * @param {object} objPokemon Les données d'un Pokémon sous forme d'objet
+	 * @returns HTMLElement Un div.pokemon
+	 */
+	static html_pokemon(objPokemon) {
+		// console.log(objPokemon);
+		var divPokemon = document.createElement("div");
+		divPokemon.classList.add("pokemon");
+		divPokemon.appendChild(this.html_nom(objPokemon.nom));
+		divPokemon.appendChild(this.html_description(objPokemon.description));
+		divPokemon.appendChild(this.html_image(objPokemon.image_url, objPokemon.nom));
+		return divPokemon;
+	}
+	/**
+	 * Fonction html_nom. Retourne le h2.nom contenant le texte fourni
+	 * @param {string} nom Le nom du Pokémon
+	 * @returns HTMLElement Un h2.nom
+	 */
+	static html_nom(nom) {
+		// console.log(nom);
+		var resultat = document.createElement("h2");
+		resultat.classList.add("nom");
+		resultat.innerHTML = nom;
+		return resultat;
+	}
+	/**
+	 * Fonction html_description. Retourne le p.description contenant le texte fourni
+	 * @param {string} description La description du Pokémon
+	 * @returns HTMLElement Un p.description
+	 */
+	static html_description(description) {
+		// console.log(description);
+		var resultat = document.createElement("p");
+		resultat.classList.add("description");
+		resultat.innerHTML = description.replace(/<br\/>/g, " ");
+		return resultat;
+	}
+	/**
+	 * Fonction html_image. Retourne le div.image contenant l'image dont l'adresse est fournie.
+	 * @param {string} image_url L'adresse de l'image
+	 * @param {string} alt Le alt à appliquer à l'image
+	 * @returns HTMLElement Un div.image
+	 */
+	static html_image(image_url, alt = "") {
+		// console.log(image_url, alt);
+		var resultat = document.createElement("div");
+		resultat.classList.add("image");
+		var img = resultat.appendChild(document.createElement("img"));
+		img.alt = alt;
+		if (image_url === "") {
+			img.src = "http://mboudrea.tim-cstj.ca/epreuve3/pokemons/pokeball.png";
+		} else {
+			img.src = image_url;
+		}
+		return resultat;
+	}
+	/**
+	 * Retourne une promesse résolue au chargement du fichier json à l'adresse fournie.
+	 * @param {string} url 
+	 * @returns json
+	 */
+	 static chargerJson(url) {
 		return new Promise(resolve => {
 			var xhr = new XMLHttpRequest();
 			xhr.open("get", url);
